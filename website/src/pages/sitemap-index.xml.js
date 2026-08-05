51e67f9b8e0cfcfc33d259d772eb869f8e88cab0
VishuPS <117966861+VishuPS@users.noreply.github.com>
2026-08-05 12:51:49 +0100

    Launch BarcodeNest website and account flow

--- website/src/pages/sitemap-index.xml.js
+++ website/src/pages/sitemap-index.xml.js
@@ -1,4 +1,4 @@
-const pages = ["", "docs/", "pricing/", "data/", "contact/", "privacy/", "terms/"];
+const pages = ["", "docs/", "pricing/", "data/", "contact/", "register/", "login/", "privacy/", "terms/"];
 export function GET() {
   const urls = pages.map((page) => `<url><loc>https://barcodenest.com/${page}</loc></url>`).join("");
   return new Response(`<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}</urlset>`, {
